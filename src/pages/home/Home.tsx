51fba3dbf696b9cc409d69a34f336c8708873fd6
towficaziz <tapolly01@gmail.com>
2024-06-12 01:05:31 +0200

    add charts in top boxes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,18 @@
+import ChartBox from "../../components/chartBox/ChartBox";
+import TopBox from "../../components/topBox/TopBox";
 import  "./home.scss";
 
 const Home = () => {
   return (
     <div className='home'>
-      <div className="box box1">box1</div>
-      <div className="box box2">box2</div>
-      <div className="box box3">box3</div>
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2"><ChartBox /></div>
+      <div className="box box3"><ChartBox /></div>
       <div className="box box4">box4</div>
-      <div className="box box5">box5</div>
-      <div className="box box6">box6</div>
+      <div className="box box5"><ChartBox /></div>
+      <div className="box box6"><ChartBox /></div>
       <div className="box box7">box7</div>
       <div className="box box8">box8</div>
       <div className="box box9">box9</div>
