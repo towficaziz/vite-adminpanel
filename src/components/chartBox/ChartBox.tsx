cffb1d0cbf58c7a184eb8b7e0859982403e99585
towficaziz <tapolly01@gmail.com>
2024-06-13 00:17:35 +0200

    adding ReChart

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -2,82 +2,92 @@ import { Link } from "react-router-dom"
 import "./chartBox.scss"
 import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts"
 
-const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+// const data = [
+//     {
+//       name: 'Page A',
+//       uv: 4000,
+//       pv: 2400,
+//       amt: 2400,
+//     },
+//     {
+//       name: 'Page B',
+//       uv: 3000,
+//       pv: 1398,
+//       amt: 2210,
+//     },
+//     {
+//       name: 'Page C',
+//       uv: 2000,
+//       pv: 9800,
+//       amt: 2290,
+//     },
+//     {
+//       name: 'Page D',
+//       uv: 2780,
+//       pv: 3908,
+//       amt: 2000,
+//     },
+//     {
+//       name: 'Page E',
+//       uv: 1890,
+//       pv: 4800,
+//       amt: 2181,
+//     },
+//     {
+//       name: 'Page F',
+//       uv: 2390,
+//       pv: 3800,
+//       amt: 2500,
+//     },
+//     {
+//       name: 'Page G',
+//       uv: 3490,
+//       pv: 4300,
+//       amt: 2100,
+//     },
+//   ];
 
-const ChartBox = () => {
+type Props = {
+  color: string;
+  icon: string;
+  title: string;
+  dataKey: string;
+  number: number | string;
+  percentage: number;
+  chartData: object[];
+}
+
+const ChartBox = (props: Props) => {
   return (
     <div className="chartBox">
       <div className="boxInfo">
         <div className="title">
-            <img src="/user.svg" alt="" />
-            <span>Total Users</span>
+            <img src={props.icon} alt="" />
+            <span>{props.title}</span>
         </div>
-        <h1>11.28</h1>
-        <Link to="">View all</Link>
+        <h1>{props.number}</h1>
+        <Link to="/" style={{color:props.color}}>View all</Link>
       </div>
       <div className="chartInfo">
         <div className="chart">
-    <ResponsiveContainer width="100%" height="100%">
-      <LineChart width={300} height={100} data={data}>
-        <Tooltip 
+    <ResponsiveContainer width="99%" height="100%">
+      <LineChart  data={props.chartData}>
+        <Tooltip
         contentStyle={{background:"transparent", border:"none"}}
         labelStyle={{display:"none"}}
         position={{x:10, y:60}}
         />
-        <Line 
+        <Line
         type="monotone" 
-        dataKey="pv" 
-        stroke="#8884d8" 
+        dataKey={props.dataKey} 
+        stroke={props.color}
         dot= {false}
         strokeWidth={2} />
       </LineChart>
     </ResponsiveContainer>
         </div>
         <div className="texts">
-            <span className="percentage">45%</span>
+            <span className="percentage" style={{color: props.percentage < 0 ? "tomato" : "lime"}}>{props.percentage}%</span>
             <span className="duration">this month   </span>
         </div>
       </div>
