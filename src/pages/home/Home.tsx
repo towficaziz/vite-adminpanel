94ae92fcf8fe529bbce68d05e2240d5e8b66bcb1
towficaziz <tapolly01@gmail.com>
2024-06-20 00:52:14 +0200

    adding revenue analytics

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import BarChartBox from "../../components/barChartBox/BarChartBox";
+import BigChartBox from "../../components/bigChartBox/BigChartBox";
 import ChartBox from "../../components/chartBox/ChartBox";
 import PieChartBox from "../../components/pieCartBox/PieChartBox";
 import TopBox from "../../components/topBox/TopBox";
@@ -16,7 +17,7 @@ const Home = () => {
       <div className="box box4"><PieChartBox/></div>
       <div className="box box6"><ChartBox {...chartBoxConversion} /></div>
       <div className="box box5"><ChartBox {...chartBoxRevenue} /></div>
-      <div className="box box7">box7</div>
+      <div className="box box7"><BigChartBox/></div>
       <div className="box box8"><BarChartBox {...barChartBoxVisit} /></div>
       <div className="box box9"><BarChartBox {...barChartBoxRevenue} /></div>
     </div>
