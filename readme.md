03fee68fd243db6df0a66e20478c051c3f736df3
towficaziz <tapolly01@gmail.com>
2024-06-21 00:21:28 +0200

    adding React MUI dataGrid

---
+++ readme.md
@@ -0,0 +1,10 @@
+
+Charts:
+
+https://recharts.org/en-US/
+
+
+Data Grid:
+
+https://mui.com/x/react-data-grid/
+
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,14 @@
+import DataTable from "../../components/dataTable/DataTable";
 import  "./users.scss";
 
 const Users = () => {
   return (
     <div className='users'>
-      Users
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New Users</button>
+      </div>
+      <DataTable/>
     </div>
   )
 }
