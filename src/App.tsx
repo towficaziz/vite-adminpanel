4a0381d236e7d8ee5f2273e387e2382b6a0959c1
towficaziz <tapolly01@gmail.com>
2024-07-03 01:20:58 +0200

    users  found succecfully using external api

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,12 @@ import Login from "./pages/login/Login";
 import "./styles/global.scss"
 import User from "./pages/user/User";
 import Product from "./pages/product/Product";
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
 
@@ -27,7 +33,9 @@ function App() {
             <Menu />
           </div>
           <div className="contentContainer">
+          <QueryClientProvider client={queryClient}>
             <Outlet />
+          </QueryClientProvider>
           </div>
         </div>
         <Footer />
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -4,6 +4,7 @@ import  "./users.scss";
 import { userRows } from "../../data";
 import { useState } from "react";
 import Add from "../../components/add/Add";
+import { useQuery } from "@tanstack/react-query";
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 50 },
@@ -55,13 +56,23 @@ const columns: GridColDef[] = [
 
 const Users = () => {
   const [open, setOpen] = useState(false)
+  
+  const { isLoading, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch("http://localhost:8800/api/users").then((res) =>
+        res.json(),
+      ),
+  })
   return (
     <div className='users'>
       <div className="info">
         <h1>Users</h1>
         <button onClick={()=>setOpen(true)}>Add New Users</button>
       </div>
-      <DataTable slug="users" columns={columns} rows={userRows}/>
+      {isLoading ? "Loading..." : (
+        <DataTable slug="users" columns={columns} rows={data}/>
+      )}
       {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   )
